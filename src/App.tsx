6fdf484d11ead3ceb2dfb8c86d298d2d1dcde5f2
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2022-11-02 22:09:53 -0500

    simple header (it sucks)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
-    return <p class='text-4xl text-green-700 text-center py-20'>Hello tailwind!</p>;
+    return (
+        <div class='flex'>
+            <div class='flex flex-col items-start px-20'>
+                <p class='text-8xl pt-20 pb-5 text-blue-400'>Fawwaz Memon</p>
+                <p class='text-2xl'>Full-Stack Developer @ Houston, Texas</p>
+            </div>
+        </div>
+    );
 };
 
 export default App;
