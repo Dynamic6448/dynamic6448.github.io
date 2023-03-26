0275a354f42b4b1d6fc82193d5c6a1f78a17a273
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2023-03-26 17:20:12 -0500

    add some cool stuff

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,24 +2,21 @@ import React from 'react';
 
 export const Experience: React.FC = () => {
     return (
-        <div className='flex flex-col bg-[#23272E] px-20'>
+        <div className='flex flex-col px-20'>
             <p className='text-7xl text-sky-500 py-10'>Experience</p>
-            <div className='flex flex-row justify-between w-full'>
-                <div className='w-1/2'>
-                    <ExperienceElement
-                        position='Project Manager & Lead Avionics Developer'
-                        place='Digital Flight Dynamics'
-                        link='https://github.com/Digital-Flight-Dynamics'
-                        start='February 2021'
-                    >
-                        An FOSS organization developing a high-fidelity Airbus A350 addon for Microsoft Flight Simulator. As a project manager and lead avionics developer, I play a critical
-                        role in creating cockpit displays that provide virtual pilots with vital information necessary for efficient flights. I also review and test code written by other
-                        members of the team, ensuring it meets our rigorous quality standards and providing feedback to help them hone their skills.
-                    </ExperienceElement>
-                </div>
+            <ExperienceElement
+                position='Project Manager & Lead Avionics Developer'
+                place='Digital Flight Dynamics'
+                link='https://github.com/Digital-Flight-Dynamics'
+                start='February 2021'
+                tools={['TypeScript', 'React', 'MSFS SDK']}
+            >
+                An FOSS organization developing a high-fidelity Airbus A350 addon for Microsoft Flight Simulator. As a project manager and lead avionics developer, I play a critical role in
+                creating cockpit displays that provide virtual pilots with vital information necessary for efficient flights. I also review and test code written by other members of the team,
+                ensuring it meets our rigorous quality standards and providing feedback to help them hone their skills.
+            </ExperienceElement>
 
-                <ExperienceElement position='Programmer' place='Texas Torque' link='https://texastorque.org' start='January 2023' />
-            </div>
+            <ExperienceElement position='Programmer' place='Texas Torque' link='https://texastorque.org' start='January 2023' tools={['Java']} />
         </div>
     );
 };
@@ -29,11 +26,12 @@ interface ExperienceElementProps {
     link: string;
     start: string;
     end?: string;
+    tools: string[];
     children?: React.ReactNode;
 }
-const ExperienceElement: React.FC<ExperienceElementProps> = ({ position, place, link, start, end = 'Present', children }) => {
+const ExperienceElement: React.FC<ExperienceElementProps> = ({ position, place, link, start, end = 'Present', tools, children }) => {
     return (
-        <div className='flex flex-col pb-10'>
+        <div className='flex flex-col pb-10 gap-1'>
             <div className='flex flex-row text-2xl'>
                 <p className='pr-5 text-gray-200'>{position}</p>
                 <a className='text-pink-700 hover:underline' href={link} target='_blank'>
@@ -43,7 +41,18 @@ const ExperienceElement: React.FC<ExperienceElementProps> = ({ position, place, 
             <p className='text-gray-300 text-xl'>
                 {start} {'->'} {end}
             </p>
-            <p className='text-lg text-slate-400'>{children}</p>
+            <p className='text-lg text-slate-400 pb-1'>{children}</p>
+            <div className='flex flex-row gap-4'>
+                {tools.map((tool) => (
+                    <Tool>{tool}</Tool>
+                ))}
+            </div>
         </div>
     );
+};
+interface ToolProps {
+    children: React.ReactNode;
+}
+const Tool: React.FC<ToolProps> = ({ children }) => {
+    return <div className='flex justify-center items-center bg-gray-700 bg-opacity-50 border-2 border-gray-600 border-opacity-50 rounded-full px-5 py-1 text-gray-200 text-lg'>{children}</div>;
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 export const Header: React.FC = () => {
     return (
         <div className='w-full h-screen'>
-            <img className='object-cover fixed select-none -z-10 brightness-75 blur-sm scale-[102%]' src='code.png' />
-            <div className='flex flex-col justify-center items-center text-center space-y-5 h-full'>
-                <p className='text-8xl sm:text-9xl text-white'>Fawwaz Memon</p>
-                <p className='text-2xl sm:text-3xl text-sky-500'>Full-Stack Developer</p>
+            <img className='object-cover fixed -z-10 brightness-75 blur-sm min-w-fit scale-[102%]' src='code.png' />
+            <div className='w-full h-full object-cover -z-10 bg-gradient-to-b from-transparent to-black opacity-75' />
+            <div className='absolute top-0 left-0 w-full h-full'>
+                <div className='flex flex-col justify-center items-center text-center space-y-5 h-full'>
+                    <p className='text-8xl sm:text-9xl text-gray-100'>
+                        <Typewriter
+                            options={{
+                                strings: ['Fawwaz Memon'],
+                                autoStart: true,
+                                loop: true,
+                                delay: 150,
+                                deleteSpeed: 75,
+                            }}
+                        />
+                    </p>
+
+                    <p className='text-2xl sm:text-3xl text-sky-500'>Full-Stack Developer</p>
+                </div>
             </div>
         </div>
     );
